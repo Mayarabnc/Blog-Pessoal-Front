53000582a54afb6f189ae5b3b2715ad65f80a5c9
Mayarabnc <mayarabncabral@gmail.com>
2022-06-08 11:13:16 -0300

    Adicionando componente tema e algumas modificações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {HttpClientModule} from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HashLocationStrategy,LocationStrategy } from '@angular/common'; /*faz com que nunca se perca entre as rotas*/
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 
@@ -20,7 +22,8 @@ import { InicioComponent } from './inicio/inicio.component';
     RodapeComponent,
     EntrarComponent,
     CadastrarComponent,
-    InicioComponent
+    InicioComponent,
+    TemaComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +31,10 @@ import { InicioComponent } from './inicio/inicio.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide:LocationStrategy,         //configura o LocationStrategy e o HashLocationStrategy,
+    useClass: HashLocationStrategy    // os transformando em objetos.
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
